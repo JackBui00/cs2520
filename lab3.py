b693bf8e2fc826e04d9273a8063b2414ba198d47
JackBui00 <jackbui05@hotmail.com>
2022-06-08 16:02:29 -0700

    Part 2 is almost done

--- lab3.py
+++ lab3.py
@@ -5,6 +5,7 @@
 #This program will return if the integer is negative, positive or 0 
 #part A 
 
+import string
 from turtle import pos
 
 
@@ -58,7 +59,27 @@ SyntaxError: positional argument follows keyword argument
 #This function is output_without_whitespace(), takes a string and prints without spaces(white space)
 import re 
 def output_without_whitespace(String):
-    
+    String = re.split(' ', String)
+
+    for x in String:
+        print(str(x), end= '')
+    return None
+#This function will create an acryonym of the phrase
+def get_acronym(String2):
+    String2 = re.split(' ', String2)
+
+    for y in String2:
+        print(str(y[0]), end='')
+    return None
+
+def main():
+    output_without_whitespace("This is a test case for the remove of the white spaces.")
+    output_without_whitespace("This is another test of the things that can happen with it")
+
+
+
+
+
 
 
 
