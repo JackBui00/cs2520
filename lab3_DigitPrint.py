588c507d4684fcf70514613552a6b285d11ae6fb
JackBui00 <jackbui05@hotmail.com>
2022-06-12 21:57:04 -0700

    Module for part 3

---
+++ lab3_DigitPrint.py
@@ -0,0 +1,26 @@
+import turtle as t
+
+def print_start_stop():
+    t.speed(1)
+    t.forward(20)
+    t.backward(20)
+    t.right(90)
+
+def print_zero():
+    t.speed(1)
+    t.forward(10)
+    t.left(90)
+    t.forward(10)
+    t.backward(10)
+    t.right(90)
+
+
+def print_one():
+    t.speed(1)
+    t.forward(10)
+    t.left(90)
+    t.forward(20)
+    t.backward(20)
+    t.right(90)
+
+
