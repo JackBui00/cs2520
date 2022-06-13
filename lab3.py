dc56e44582666d17b2b39b057fd88ed8aac19f94
JackBui00 <jackbui05@hotmail.com>
2022-06-12 22:43:56 -0700

    Finish lab 3 for print barcode

--- lab3.py
+++ lab3.py
@@ -62,7 +62,6 @@ import re
 def output_without_whitespace(String = "This is a test case for the remove of the white spaces.\n"):
     String = re.split(' ', String)
     final_result_one = ''
-    print(String)
     for x in String:
         final_result_one = final_result_one + str(x)
         #print(str(x), end= '')
@@ -75,7 +74,7 @@ def get_acronym(String2 = "This is another test of the things that can happen wi
     for y in String2:
         final_result_two = final_result_two + str(y[0])
         #print(str(y[0]), end='')
-    return final_result_two
+    return final_result_two.upper()
 
 def main():
     user_input_one = str(input("\nEnter the first sentence to remove all the white space: "))
@@ -84,10 +83,47 @@ def main():
     print(get_acronym(user_input_two))
 
 main()
+#output
+'''
+Enter the first sentence to remove all the white space: The only thing we have to fear is fear itself.
+Theonlythingwehavetofearisfearitself.
 
+Enter the phrase that you want an acronym for: random access memory
+RAM
+'''
 
+#Lab 3 – Task 3
 
+import lab3_DigitPrint as dp
 
+#Take user input for the barcode
+def take_barcode():
+    barcode = input("Input a barcode: ")
+    barcode = barcode.replace('-','')
+    return barcode
+
+#Splices and creates a list of the zipcode values, including the check_sum value to the end 
+def check_sum(barcode):
+    barcode_breakdown=[]
+    for integer in barcode:
+        barcode_breakdown.append(int(integer))
+    barcode_breakdown.append(10 - sum(barcode_breakdown) % 10)
+    return barcode_breakdown
+
+#draws out the barcode that's to be created with the key 
+def print_barcode_main(barcode_breakdown):
+    key =[[1,1,0,0,0],[0,0,0,1,1],[0,0,1,0,1],[0,0,1,1,0],[0,1,0,0,1],[0,1,0,1,0],[0,1,1,0,0],[1,0,0,0,1],[1,0,0,1,0],[1,0,1,0,0]]
+    dp.print_start()
+    for value in barcode_breakdown:
+        for value_two in key[value]:
+            if value_two == 0:
+                dp.print_zero()
+            else:
+                dp.print_one()
+    dp.print_end()
+
+print_barcode_main(check_sum(take_barcode()))
+print_barcode_main(check_sum(take_barcode()))
 
 
 
