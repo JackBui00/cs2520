8b3b5bf8899301cc814f6ee51e5423d9720c3059
JackBui00 <jackbui05@hotmail.com>
2022-06-12 22:43:28 -0700

    Removed the test function

--- lab3_DigitPrint.py
+++ lab3_DigitPrint.py
@@ -42,22 +42,4 @@ def print_one():
     t.right(90)
 
 
-def print_barcode(barcode):
-    barcode = barcode.replace('-','')
-    key =[[1,1,0,0,0],[0,0,0,1,1],[0,0,1,0,1],[0,0,1,1,0],[0,1,0,0,1],[0,1,0,1,0],[0,1,1,0,0],[1,0,0,0,1],[1,0,0,1,0],[1,0,1,0,0]]
-    barcode_breakdown = []
-    for integer in barcode:
-        barcode_breakdown.append(int(integer))
-    print(barcode_breakdown)
-    print(sum(barcode_breakdown))
-    sum_of_barcode = sum(barcode_breakdown)
-    barcode_breakdown.append(10 - sum_of_barcode % 10)
-    print(barcode_breakdown)
-    print_start()
-    for value in barcode_breakdown:
-        for value_two in key[value]:
-            if value_two == 0:
-                print_zero()
-            else:
-                print_one()
-    print_end()
+
