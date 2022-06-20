94af3d91d53a213dd80018356fdcd43cf0d0d90f
JackBui00 <jackbui05@hotmail.com>
2022-06-20 14:58:30 -0700

    file read and export to list task 1 part 1

--- lab5.py
+++ lab5.py
@@ -1,1 +1,26 @@
-l
+#Name: Jack Bui 
+
+#Lab 5 – Task 1
+
+
+def open_file_read(file_name):
+    target = open(file_name, 'r')
+    return target
+
+def open_file_write(file_name):
+    target = open(file_name, 'w')
+    return target
+
+def open_file_append(file_name):
+    target = open(file_name, 'a')
+    return target
+
+def main():
+    names_and_values = []
+    scores = open_file_read("scores.txt")
+    for line in scores:
+        names_and_values.append(line.split())
+    print(names_and_values)
+
+
+main()
