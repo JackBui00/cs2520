2ea634e30f85ee8812217b63d3f7e9789305e001
JackBui00 <jackbui05@hotmail.com>
2022-06-25 17:59:54 -0700

    lab6 finish

--- lab6.py
+++ lab6.py
@@ -1,4 +1,4 @@
-#Name: Jack Bui 
+#Name: Jack Bui
 
 #Lab 6 – Task 1
 class   question():
@@ -10,7 +10,7 @@ class   question():
         for key in self.dict.values():
             print(key)
         return self.check_answer()
-        
+
     def check_answer(self):
         user_input = input()
         if user_input.lower() == self.correct_answer:
@@ -18,36 +18,15 @@ class   question():
         else:
             return False
 
-class   question1():
-    question_dict = {"question" : "Where is Cal Poly Pomona located?", "a" : "A. Texas" , "b" : "B. California",
-         "c" : "C. Nevada", "d" : "D. Washington"}
-    answer = "b"
+class test():
+    def __init__(self, list):
+        self.list = list
 
-class   question2():
-    question_dict = {"question" : "On your student records system, what does CSU stand for?", "a" : "A. California State Universities" ,
-    "b" : "B. Colorada State University", "c" : "C. Color Sparkling Unicorn", "d" : "D. Canadian Swan Upping"}
-    answer = "a"
-
-class   question3():
-    question_dict = {"question" : "What is 5 + 5?", "a" : "A. 23" , "b" : "B. 15", "c" : "C. -23", "d" : "D. 10"}
-    answer = "d"
-
-class   question4():
-    question_dict = {"question" : "How many moons does Earth have?", "a" : "A. 32" , "b" : "B. 3", "c" : "C. 1", "d" : "D. 0"}
-    answer = "c"
-
-q1 = question({"question" : "Where is Cal Poly Pomona located?", "a" : "A. Texas" , "b" : "B. California",
-         "c" : "C. Nevada", "d" : "D. Washington"}, "b")
-
-q2 = question({"question" : "On your student records system, what does CSU stand for?", "a" : "A. California State Universities" ,
-    "b" : "B. Colorada State University", "c" : "C. Color Sparkling Unicorn", "d" : "D. Canadian Swan Upping"}, "a")
-
-q3 = question({"question" : "What is 5 + 5?", "a" : "A. 23" , "b" : "B. 15", "c" : "C. -23", "d" : "D. 10"}, "d")
-
-q4 = question({"question" : "How many moons does Earth have?", "a" : "A. 32" , "b" : "B. 3", "c" : "C. 1", "d" : "D. 0"}, "c")
-
-q5 = question({"question": "What lab is this?", "a" : "A. 3" , "b" : "B. 1", "c" : "C. 4", "d" : "D. 6"}, "d")
-
+    def take_test(self, student):
+        print("\nWelcome to the test", student.name)
+        for dictionary in self.list:
+            if dictionary.print_question() == True:
+                student.correct()
 
 
 class   student():
@@ -69,8 +48,146 @@ class   student():
     def reset(self):
         self.test_score = 0 
 
-input = "c"
-q1 = question1()
 
-p1 = student("jack", 13, 1234)
 
+def main():
+    q1 = question({"question" : "Where is Cal Poly Pomona located?", "a" : "A. Texas" , "b" : "B. California",
+         "c" : "C. Nevada", "d" : "D. Washington"}, "b")
+
+    q2 = question({"question" : "On your student records system, what does CSU stand for?", "a" : "A. California State Universities" ,
+    "b" : "B. Colorada State University", "c" : "C. Color Sparkling Unicorn", "d" : "D. Canadian Swan Upping"}, "a")
+
+    q3 = question({"question" : "What is 5 + 5?", "a" : "A. 23" , "b" : "B. 15", "c" : "C. -23", "d" : "D. 10"}, "d")
+
+    q4 = question({"question" : "How many moons does Earth have?", "a" : "A. 32" , "b" : "B. 3", "c" : "C. 1", "d" : "D. 0"}, "c")
+
+    q5 = question({"question": "What lab is this?", "a" : "A. 3" , "b" : "B. 1", "c" : "C. 4", "d" : "D. 6"}, "d")
+
+    
+    test1 = test([q1,q2,q3,q4,q5])
+    
+    p1 = student("jack", 13, 1234)
+    p2 = student("james", 17, 4321)
+    p3 = student("jacob", 15, 2341)
+
+    test1.take_test(p1)
+    p1.print_values()
+    test1.take_test(p2)
+    p2.print_values()
+    test1.take_test(p3)
+    p3.print_values()
+
+if __name__ == "__main__":
+    main()
+
+
+#output
+"""
+Welcome to the test jack
+Where is Cal Poly Pomona located?
+A. Texas
+B. California
+C. Nevada
+D. Washington
+b
+On your student records system, what does CSU stand for?
+A. California State Universities
+B. Colorada State University
+C. Color Sparkling Unicorn
+D. Canadian Swan Upping
+a
+What is 5 + 5?
+A. 23
+B. 15
+C. -23
+D. 10
+d
+How many moons does Earth have?
+A. 32
+B. 3
+C. 1
+D. 0
+c
+What lab is this?
+A. 3
+B. 1
+C. 4
+D. 6
+d
+Name: jack
+Age: 13
+ID: 1234
+Test Score: 5
+
+Welcome to the test james
+Where is Cal Poly Pomona located?
+A. Texas
+B. California
+C. Nevada
+D. Washington
+a
+On your student records system, what does CSU stand for?
+A. California State Universities
+B. Colorada State University
+C. Color Sparkling Unicorn
+D. Canadian Swan Upping
+b
+What is 5 + 5?
+A. 23
+B. 15
+C. -23
+D. 10
+d
+How many moons does Earth have?
+A. 32
+B. 3
+C. 1
+D. 0
+c
+What lab is this?
+A. 3
+B. 1
+C. 4
+D. 6
+a
+Name: james
+Age: 17
+ID: 4321
+Test Score: 2
+
+Welcome to the test jacob
+Where is Cal Poly Pomona located?
+A. Texas
+B. California
+C. Nevada
+D. Washington
+c
+On your student records system, what does CSU stand for?
+A. California State Universities
+B. Colorada State University
+C. Color Sparkling Unicorn
+D. Canadian Swan Upping
+c
+What is 5 + 5?
+A. 23
+B. 15
+C. -23
+D. 10
+c
+How many moons does Earth have?
+A. 32
+B. 3
+C. 1
+D. 0
+c
+What lab is this?
+A. 3
+B. 1
+C. 4
+D. 6
+c
+Name: jacob
+Age: 15
+ID: 2341
+Test Score: 1
+"""
