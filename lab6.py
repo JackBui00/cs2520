b1cd28dd13bce7c449f442dce3e64e5277bc7184
JackBui00 <jackbui05@hotmail.com>
2022-06-25 17:24:40 -0700

    classes change

--- lab6.py
+++ lab6.py
@@ -1,8 +1,18 @@
 #Name: Jack Bui 
 
 #Lab 6 – Task 1
+class   question():
+    def __init__(self, dict, correct_answer):
+        self.dict = dict
+        self.correct_answer = correct_answer
 
+    def print_question(self):
+        for key in self.dict.values():
+            print(key)  
 
+    def check_answer(self, input):
+        if input.lower() == self.correct_answer:
+            return True
 class   question1():
     question_dict = {"question" : "Where is Cal Poly Pomona located?", "a" : "A. Texas" , "b" : "B. California",
          "c" : "C. Nevada", "d" : "D. Washington"}
@@ -17,13 +27,17 @@ class   question3():
     question_dict = {"question" : "What is 5 + 5?", "a" : "A. 23" , "b" : "B. 15", "c" : "C. -23", "d" : "D. 10"}
     answer = "d"
 
-def print_question(target):
-        for key in target:
-            print(key)  
+class   question4():
+    question_dict = {"question" : "How many moons does Earth have?", "a" : "A. 32" , "b" : "B. 3", "c" : "C. 1", "d" : "D. 0"}
+    answer = "c"
 
-def check_answer(input, answer):
-    if input.lower() == answer:
-        return True
+q1 = question({"question" : "Where is Cal Poly Pomona located?", "a" : "A. Texas" , "b" : "B. California",
+         "c" : "C. Nevada", "d" : "D. Washington"}, "b")
+
+q1.print_question()
+
+print(q1.check_answer("c"))
+
 
 class   student():
     def __init__(self, name, age, id, test_score = 0):
@@ -44,5 +58,8 @@ class   student():
     def reset(self):
         self.test_score = 0 
 
+input = "c"
+q1 = question1()
 
 p1 = student("jack", 13, 1234)
+
