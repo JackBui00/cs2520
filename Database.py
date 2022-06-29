3fccc060e635d051f4638475f553308c15ee0ec2
jackbui05 <jackbui05@hotmail.com>
2022-06-28 17:29:31 -0700

    admin create new user and password

--- Database.py
+++ Database.py
@@ -1,5 +1,7 @@
 import PySimpleGUI as sg
 from cryptography.fernet import Fernet
+from sympy import *
+
 
 encryption_key = Fernet.generate_key()
     
@@ -28,20 +30,27 @@ def main():
     ]
     window = sg.Window('Login Portal', layout)
 
-    database = {"jack": "password","james": "password123", "german": "potato"}
+    database = {"jack": "wordpass","james": "password123", "german": "potato"}
 
     database = encrypt(database)
 
     while True:             # Event Loop
+        
         event, value = window.read()
         #print( event, value)
         if event == sg.WIN_CLOSED or event == 'Exit':
             break
         if event == 'Enter':
-            if value[0].lower() not in database.keys() or value[1] != fernet.decrypt(database[value[0]]).decode():
+            if "admin" in value[0] and "password" in value[1]:
+                return_statement.update(value ="New Username and Password Created")
+                new_username = value[0].split(" ")[1]
+                new_password = value[1].split(" ")[1]
+                database[new_username] = fernet.encrypt(new_password.encode())
+            elif value[0] in database.keys():
+                if fernet.decrypt(database[value[0]]).decode() == value[1]:
+                    return_statement.update(value = "The password was correct")
+            elif value[0].lower() not in database.keys() or value[1] != fernet.decrypt(database[value[0]]).decode():
                 return_statement.update(value="The Username or password you entered does not exist or is incorrect")
-            elif fernet.decrypt(database[value[0]]).decode() == value[1]:
-                return_statement.update(value = "The password was correct")
         
     window.close()
     print(database)
