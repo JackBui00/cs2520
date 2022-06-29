b90492f8d4c58e881b362933fd2f38657c157126
jackbui05 <jackbui05@hotmail.com>
2022-06-28 17:07:15 -0700

    return statement

--- Database.py
+++ Database.py
@@ -2,23 +2,24 @@ import PySimpleGUI as sg
 
 
 def main():
+    return_statement = sg.Text()
     layout = [
         [sg.Text("Login Screen")],
         [sg.Text("Username",size = (15,1)), sg.InputText()],
         [sg.Text("Password",size = (15,1)), sg.InputText()],
+        [return_statement],
         [sg.Button("Enter"), sg.Button("Exit")]
     ]
     window = sg.Window('Login Portal', layout)
-    database = {}
+    database = {"jack": "password"}
     while True:             # Event Loop
         event, value = window.read()
         #print( event, value)
         if event == sg.WIN_CLOSED or event == 'Exit':
             break
         if event == 'Enter':
-            if value[0] not in database.keys():
-                print("Does not exist")
-                database[value[0]] = value[1]
+            if value[0] not in database.keys() or value[1] != database[value[0]]:
+                return_statement.update(value="The Username or password you entered does not exist or is incorrect")
         
     window.close()
     print(database)
