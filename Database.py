13dc7fd6b1a8d8395d3a2c02c620ca4fdd602a56
jackbui05 <jackbui05@hotmail.com>
2022-07-01 14:26:48 -0700

    Create New User Window

--- Database.py
+++ Database.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
 from cryptography.fernet import Fernet
 from sympy import *
+import csv  #maybe csv file
 
 
 encryption_key = Fernet.generate_key()
@@ -19,6 +20,34 @@ def decrypt(target):
         target[v] = fernet.decrypt(target[v]).decode()
     return target
 
+def make_new_user(database):
+    return_statement = sg.Text()
+    layout = [
+        [sg.Text("Make New User")],
+        [sg.Text("Username",size = (10,1)), sg.InputText()],
+        [sg.Text("Password",size = (10,1)), sg.InputText()],
+        [return_statement],
+        [sg.Button("Enter"), sg.Button("Exit")]
+    ]
+    window = sg.Window("Create A New User", layout)
+    while True:             # Event Loop
+        
+        event, value = window.read()
+        #print( event, value)
+        if event == sg.WIN_CLOSED or event == "Exit":
+            break
+        if event == "Enter":
+            if value[0] in database.keys():
+                return_statement.update(value = "Username already exist")
+                continue
+            database[value[0]] = fernet.encrypt(value[1].encode())
+            return_statement.update(value = "New User has been made")
+    
+    window.close()
+
+
+
+
 def main():
     return_statement = sg.Text()
     layout = [
@@ -29,7 +58,7 @@ def main():
         [sg.Button("Enter"), sg.Button("Exit"),sg.Button("Make New User"),sg.Button("Forgot Password")]
     ]
     window = sg.Window("Login Portal", layout)
-    
+
     database = {"jack": "wordpass","james": "password123", "german": "potato"}
 
     database = encrypt(database)
@@ -46,8 +75,9 @@ def main():
                     return_statement.update(value = "The password was correct")
             elif value[0].lower() not in database.keys() or value[1] != fernet.decrypt(database[value[0]]).decode():
                 return_statement.update(value="The Username or password you entered does not exist or is incorrect")
-        if event == "Name New User":
-            database[value[0]] = fernet.encrypt(value[1].encode())
+        if event == "Make New User":
+            make_new_user(database)
+            
         if event == "Forgot Password":
             return_statement.update(value = fernet.decrypt(database[value[0]]).decode())
 
